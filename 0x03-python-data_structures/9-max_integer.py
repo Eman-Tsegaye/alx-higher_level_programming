e115f5e1aeab784f278b84022d8a8df0ad3b7dda
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-11 18:01:48 +0000

    commiting 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list=[]):
     n = len(my_list)
     bigger = my_list[0]
+    if n == 0:
+        return None
     if n != 0:
         for i in my_list:
             if i > bigger:
                 bigger = i
         return(bigger)
-    if n == 0
-        return None
