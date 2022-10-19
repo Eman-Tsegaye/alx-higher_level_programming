c4d472543b6c718c7c32ff84d8b379eb4e96776b
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-19 06:53:54 +0000

    commiting 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''This module contains a class named Square'''
+
+
+class Square:
+
+    '''This Class has an attribute named size'''
+
+    def __init__(self, size):
+        '''This method initializes this class and sets size'''
+
+        self.__size = size
