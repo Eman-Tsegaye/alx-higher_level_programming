3676e3750e85bbaf4241fcd95b9e13b4289ce1c2
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-19 07:54:02 +0000

    commiting 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
     def __init__(self, size=0):
         '''This method initializes this class and sets size'''
         self.__size = size
-    
+
     def area(self):
         '''This method calculates and returns the area of a square'''
         return (self.__size ** 2)
