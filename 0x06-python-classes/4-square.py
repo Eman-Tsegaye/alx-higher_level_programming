00f5acc0e2f1eee2577fe016245a09b3381f9d86
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-19 07:41:08 +0000

    commiting 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+'''This module contains a class named Square'''
+
+
+class Square:
+
+    '''This Class has an attribute named size'''
+
+    def __init__(self, size=0):
+        '''This method initializes this class and sets size'''
+        self.__size = size
+
+    def area(self):
+        '''This method calculates and returns the area of a square'''
+        return (self.__size ** 2)
+
+    def size(self):
+        '''A getter method'''
+        return self.__size
+
+    def size(self, value):
+        '''A setter method'''
+        if (type(size) != int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
