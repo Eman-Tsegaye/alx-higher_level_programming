13f99091a61ca4706d3848bc8c147696c8f6e505
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-26 14:44:12 +0000

    commiting 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a Square class that inherits from Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square class"""
+
+    def __init__(self, size):
+        """Initialization of a square"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
