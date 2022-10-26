7b1635463dcea64965eabbd6262cf7f89a7a9372
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-26 13:24:46 +0000

    commiting 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""contains the checker function"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is exactly an instance of the specified class"""
+    if type(obj) == a_class:
+        return True
+    return False
