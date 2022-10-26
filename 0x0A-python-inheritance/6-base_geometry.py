add5bee0998cd14e6b002bd691a89e6303dbcd94
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-10-26 13:50:52 +0000

    commiting 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """base geometry"""
+
+    def area(self):
+        """raises an Exception"""
+
+        raise Exception('area() is not implemented')
