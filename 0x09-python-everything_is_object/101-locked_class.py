8baa40958b0d0025aeaee3cf65424846203f8a4e
Eman-Tsegaye <emantsegaye3626@gmail.com>
2022-11-29 13:39:35 +0000

    commiting answers to all questions

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a Locked Class"""
+
+
+class LockedClass():
+    """Representation of class
+    """
+    __slots__ = ("first_name")
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,2 @@
+""This directory contains solutions for the project 0x09. Python - Everything is object"
+"Done by Eman Tsegaye"
